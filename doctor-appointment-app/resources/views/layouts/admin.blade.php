dab3e3df3a58b639287c685fe838a3d674d63d9f
Jorge Emmanuel Ruiz Castilla <jorgeaguila440@gmail.com>
2025-10-15 17:58:51 -0600

    feat(layout): add breadcrumb  component and integrate into admin layout

--- doctor-appointment-app/resources/views/layouts/admin.blade.php
+++ doctor-appointment-app/resources/views/layouts/admin.blade.php
@@ -1,38 +1,55 @@
+@props(['breadcrumbs' => []])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.bunny.net">
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script src="https://kit.fontawesome.com/a7de8752fc.js" crossorigin="anonymous"></script>
+<!-- Scripts -->
+@vite(['resources/css/app.css', 'resources/js/app.js'])
+<script src="https://kit.fontawesome.com/9161014f5f.js" crossorigin="anonymous"></script>
 
-        <wireui:scripts />
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased bg-gray-50">
-        @include('layouts.includes.admin.navigation')
+<!-- Styles -->
+<head>
+    ...
+    <wireui:scripts />
+    <script src="//unpkg.com/alpinejs" defer></script>
+</head>
+@livewireStyles
 
-        @include('layouts.includes.admin.sidebar')
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <div class="p-4 sm:ml-64">
-            <!-- Margin top 14px -->
-            <div class="mt-14">
-                {{ $slot }}
-            </div>
-        </div>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        @stack('modals')
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+<body class="font-sans antialiased bg-gray-50">
+@include('layouts.includes.admin.navigation')
+@include('layouts.includes.admin.sidebar')
 
-        @livewireScripts
-    </body>
+<div class="p-4 sm:ml-64">
+    {{-- añadiendo margen superior --}}
+    <div class="mt-14 flex items-center justify-between w-full">
+        @include('layouts.includes.admin.breadcrumb')
+    </div>
+    {{ $slot }}
+</div>
+
+@stack('modals')
+
+@livewireScripts
+<script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+
+</body>
+</html>
